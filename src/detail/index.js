ec433a45227c9e4a300cdaf290b3dba014219b38
libin_li <libin_li@youzan.com>
2022-05-17 18:04:32 +0800

    fix: 增加首页

--- src/detail/index.js
+++ src/detail/index.js
@@ -1,4 +1,8 @@
+import { useEffect } from "react"
+
 export default function App() {
+  useEffect(() => console.log(555), []);
+
   return (
     <div>weather1111</div>
   )
--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,32 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import HeaderComp from './components/header-comp';
+import ButtomComp from './components/bottom-comp';
+import Card from './components/card';
+import InfoList from './components/info-list';
+import './style.scss';
+
 export default function App() {
+  const [weatherData, setWeatherData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('https://restapi.amap.com/v3/weather/weatherInfo?city=330100&key=1bc7cd5d9f62b610161eeb206b5f4d47')
+      .then((res) => {
+        const { status, data } = res;
+
+        if (status === 200) {
+          setWeatherData(data.lives[0]);
+        }
+      })
+  }, []);
+
   return (
-    <div>weather</div>
+    <div className="dashbord">
+      <HeaderComp />
+      { weatherData.weather ? <Card weather={weatherData} /> : null }
+      { weatherData.weather ? <InfoList weather={weatherData} /> : null }
+      <ButtomComp />
+    </div>
   )
 }
